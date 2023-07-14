30fae988f0b7b7aae73136429a494e2763556dfa
Emrah <emrahilgar62@gmail.com>
2023-07-14 15:58:47 +0200

    day1

---
+++ src/test/java/com/eurotech/tests/day1_intro/OpenBrowser.java
@@ -0,0 +1,20 @@
+package com.eurotech.tests.day1_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class OpenBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        driver.close();
+        driver=new FirefoxDriver();
+        driver.get("https://www.google.com/");
+    }
+}
